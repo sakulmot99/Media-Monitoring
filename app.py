6cc5850926a4a625753a50567c1bbbb83704867e
sakulmot99 <Lukas.k.brand99@gmail.com>
2026-01-07 21:40:56 +0100

    Refactor app initialization and server setup

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
-from dashboard.dash_app import create_dash_app
+import os
+from dashboard.dash_app import app
 
-app = create_dash_app()
-server = app.server  # Required for deployment
+# Gunicorn needs this
+server = app.server
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8050)),
+        debug=False
+    )
